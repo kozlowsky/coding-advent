91de43eb2318abf104914d6a24728a95df792f2d
Jakub Kozlowski <jakub.kozlowski@p3-group.com>
2019-12-07 16:14:45 +0100

    day one part two impl

--- src/dayOne/Solution.kt
+++ src/dayOne/Solution.kt
@@ -5,16 +5,31 @@ import shared.Utils
 fun main(args: Array<String>) {
     val solution = Solution()
 
-    println("Solution: ${solution.run(Utils.readTextFile("input"))}")
+    println("Solution: ${solution.runPartOne(Utils.readTextFile("input"))}")
+    println("Solution: ${solution.runPartTwo(Utils.readTextFile("input"))}")
 }
 
 class Solution {
 
-    fun run(values: ArrayList<String>): Int {
-        return values.map {
+    fun runPartOne(values: ArrayList<String>): Int {
+        return values.sumBy {
             calculateRequiredFuel(it.toInt())
-        }.sum()
+        }
     }
+
+    fun runPartTwo(values: ArrayList<String>): Int {
+        return values.sumBy {
+            calculateRequiredFuel(it.toInt(), 0)
+        }
+    }
+
+    private fun calculateRequiredFuel(mass: Int, acc: Int): Int {
+        val requiredFuel = calculateRequiredFuel(mass)
+        return if (requiredFuel > 0)
+            calculateRequiredFuel(requiredFuel, acc + requiredFuel)
+        else acc
+    }
+
 
     private fun calculateRequiredFuel(mass: Int): Int {
         return (mass / 3) - 2
